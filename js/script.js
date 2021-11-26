a3401907e6d52841d7e5072000d1e5957c45c2b3
bardachyounes <bardach.younes@gmail.com>
2021-11-26 01:39:11 +0100

    added a script for a single round of rock paper scissors between a player and a computer

--- js/script.js
+++ js/script.js
@@ -1,23 +1,46 @@
 const rock = "rock";
 const scissors = "scissors";
 const paper = "paper";
+let randomChoice = Math.floor(Math.random() * 3);
 
 function computerPLay() {
-    let randomChoice = Math.floor(Math.random() * 3);
     switch (randomChoice) {
         case 0:
-            console.log(rock);
+            randomChoice = "rock"
             break;
 
         case 1:
-            console.log(paper);
+            randomChoice = "paper"
             break;
 
         case 2:
-            console.log(scissors);
+            randomChoice = "scissors"
             break;
     }
 }
 
 computerPLay()
+
+const computerSelection = randomChoice;
+let playerImput = "scissoRS";
+const playerSelection = playerImput.toLowerCase();
+
+const draw = "draw";
+const win = `you win, ${playerSelection} beats ${computerSelection}`
+const lose = `you lose, ${computerSelection} beats ${playerSelection}`
+
+//scissors beats paper
+//paper beats rock
+//rock beats scissors
+
+function singleRound(a, b) {
+if (a === b) {return draw}
+if ((a === scissors && b === paper) || (a === paper && b === rock) || (a === rock && b === scissors)) {return lose}
+else {return win}
+}
+
+singleRound(computerSelection, playerSelection)
+console.log (`computer chose ${computerSelection}`)
+console.log (`you chose ${playerSelection}`)
+console.log(singleRound(computerSelection, playerSelection))
 
